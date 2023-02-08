48aa98ac1675dea51598d2bbc3f460c5e50d58ec
zziyad <zziyadkhanov@gmail.com>
2023-02-08 15:25:40 +0400

    fix the bugs

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ const fromInput = document.querySelector('.currency_from input');
 const fromInputOutput = document.querySelector('.currency_from div');
 const toInput = document.querySelector('.currency_to input');
 const toInputOutput = document.querySelector('.currency_to div');
+
+
 let v1 = 'RUB';
 let v2 = 'EUR';
 let rate = 0;
@@ -19,7 +21,8 @@ let rate = 0;
 fromInput.addEventListener('input', (e) => {
   let v = 1;
   if (e.target.value.trim() !== '') {
-    v = parseFloat(e.target.value.trim());
+    if (!isNaN(parseFloat(e.target.value))) v = parseFloat(e.target.value.trim());
+    return 
   }
   toInput.value = Math.trunc(rate * v * 100) / 100;
 });
@@ -28,43 +31,44 @@ fromInput.addEventListener('input', (e) => {
 toInput.addEventListener('input', (e) => {
   let v = 1;
   if (e.target.value.trim() !== '') {
-    v = parseFloat(e.target.value.trim());
+    if (!isNaN(parseFloat(e.target.value))) v = parseFloat(e.target.value.trim());
+    
   }
   fromInput.value = Math.trunc((1 / rate) * v * 100) / 100;
 });
 
 for (const elem of div.children) {
   elem.addEventListener('click', () => {
+    if (elem.classList[1] === 'active' ) return;
     [...div.children].forEach(el => el.classList.remove('active'))
     elem.classList.toggle("active");
     v1 = elem.textContent
-    getExchange(v1, v2)
+     getExchange(v1, v2);
+    // return getExchange(v1, v2);
   })
-}
+};
+
 for (const elem of div2.children) {
   elem.addEventListener('click', () => {
+    if (elem.classList[1] === 'active' ) return;
     [...div2.children].forEach(el => el.classList.remove('active'))
     elem.classList.toggle("active");
     v2 = elem.textContent
-    getExchange(v1, v2)
+     getExchange(v1, v2);
   })
-}
+};
+
 async function getExchange(v1 = 'RUB', v2 = 'EUR') {
-
-
-  const rate2 = await fetch(URL + `${v2}&symbols=${v1}`).then((r) => r.json()).then((res) => {
-    return  res.rates[v1];
-  })
-
-  fetch(URL + `${v1}&symbols=${v2}`)
+  fetch(URL + `${v1}`)
     .then((data) => data.json()).then((result) => {
+      
       rate = result.rates[v2];
       let v = fromInput.value.trim();
       if (v !== '') v = parseFloat(v);
       else v = 1;
       fromInputOutput.innerHTML = `1 ${v1} = ${rate} ${v2}`
-      toInputOutput.innerHTML = `1 ${v2} = ${ rate2} ${v1}`
-      toInput.value = rate * v;
+      toInputOutput.innerHTML = `1 ${v2} = ${result.rates[v1]} ${v1}`
+      toInput.value = Math.trunc(rate * v * 100) / 100;;
     });
 
 };
